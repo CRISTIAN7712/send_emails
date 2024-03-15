1a68ef73d9e56c9f893f5bd0cc31732795a4df1f
CRISTIAN7712 <cristian.diaz8918@gmail.com>
2024-03-15 12:26:01 -0500

    requierements implementados

--- send_mail.py
+++ send_mail.py
@@ -4,11 +4,14 @@ import os
 from dotenv import load_dotenv
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+from flask_cors import CORS
+
 
 # Cargar las variables de entorno desde el archivo .env
 load_dotenv()
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/send_mail', methods=['POST'])
 def send_mail():
@@ -25,15 +28,21 @@ def send_mail():
     msg = MIMEMultipart()
     msg['From'] = user
     msg['Subject'] = subject
-    msg['To'] = ','.join(recipients)
+    msg['To'] = ', '.join(recipients)
     msg.attach(MIMEText(body))
 
     with smtplib.SMTP('smtp.gmail.com', 587) as server:
         server.starttls()
         server.login(user_mail, password)
-        server.sendmail(user_mail, recipients, msg.as_string())
+        
+        for recipient in recipients:
+            # Enviar el correo a cada destinatario individualmente
+            server.sendmail(user_mail, recipient, msg.as_string())
 
-    return jsonify({'message': 'Correo enviado con exito'})
+    return jsonify({'message': 'Correo enviado con éxito'})
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=5000, debug=True)
